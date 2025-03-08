dae76f1b488c79294015512061442b8ba7b1e59b
Rahiman Shaik <47222515+sshaik71@users.noreply.github.com>
2025-03-08 11:36:40 +0000

    updated

--- main.tf
+++ main.tf
@@ -105,7 +105,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_node_group.arn
-  subnet_ids      = data.aws_subnets.public.ids
+  subnet_ids      = [data.aws_subnets.public.ids[1]]
 
   scaling_config {
     desired_size = 2
